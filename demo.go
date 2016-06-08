1c9fa544d47d03792668a9b0b58b57d79a64c4ce
陈光临 <chenguanglin@beiquan.org>
2016-06-08 15:44:29 +0800

    init

---
+++ demo.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+//插入排序
+func insertion_sort(unsorted []int) {
+	fmt.Println("排序前")
+	fmt.Println(unsorted)
+
+	for i := 1; i < len(unsorted); i++ {
+		key := unsorted[i]
+
+		var j int
+
+		for j = i - 1; j >= 0; j-- {
+			if key < unsorted[j] { //查找合适的位置
+				unsorted[j+1] = unsorted[j]
+			} else {
+				break
+			}
+		}
+		unsorted[j+1] = key
+	}
+
+	fmt.Println("排序后")
+	fmt.Println(unsorted)
+}
+
+//桶排序
+func bucket_sort(unsorted []int, max int) {
+	fmt.Println("排序前")
+	fmt.Println(unsorted)
+
+	a := make([]int, max+1)
+
+	for i := 0; i < len(unsorted); i++ {
+		key := unsorted[i]
+		a[key]++
+	}
+
+	index := 0
+	for i := 0; i < len(a); i++ {
+		for j := 0; j < a[i]; j++ {
+			unsorted[index] = i
+			index++
+		}
+	}
+
+	fmt.Println("排序后")
+	fmt.Println(unsorted)
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+//	"fmt"
+)
+
+func main() {
+	//	fmt.Println("hello world")
+
+	a := []int{2, 5, 11, 9, 7, 1, 0, 11, 83, 9, 24}
+
+	//	insertion_sort(a)
+
+	bucket_sort(a, 83)
+
+	//	var a [5]int = [2,3,8,6,5]
+
+} ///Users/chenguanglin/mygo/main.go
